9e44159c35652268c57d9378218ded15451d3dfc
Jmiguel14 <miguel.jurado@epn.edu.ec>
2021-09-25 17:49:54 -0500

    course information step 3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,45 +2,49 @@ const Head = ({ course }) => {
   return <h1>{course}</h1>;
 };
 
-const Part1 = ({part1, exercises1}) => {
-  return <p> {part1} {exercises1} </p>
+const Part1 = ({part1}) => {
+  return <p> {part1.name} {part1.exercises} </p>
 }
-const Part2 = ({part2, exercises2}) => {
-  return <p> {part2} {exercises2} </p>
+const Part2 = ({part2}) => {
+  return <p> {part2.name} {part2.exercises} </p>
 }
-const Part3 = ({part3, exercises3}) => {
-  return <p> {part3} {exercises3} </p>
+const Part3 = ({part3}) => {
+  return <p> {part3.name} {part3.exercises} </p>
 }
 
 const Content = ({
   part1,
   part2,
   part3,
-  exercises1,
-  exercises2,
-  exercises3,
 }) => {
   return (
     <>
-      <Part1 part1={part1} exercises1={exercises1}/>
-      <Part2 part2={part2} exercises2={exercises2}/>
-      <Part3 part3={part3} exercises3={exercises3}/>
+      <Part1 part1={part1} />
+      <Part2 part2={part2} />
+      <Part3 part3={part3} />
     </>
   );
 };
 
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+const Total = ({ part1, part2, part3 }) => {
+  return <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>;
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = 'Half Stack application development'
+
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
@@ -49,14 +53,11 @@ const App = () => {
         part1={part1}
         part2={part2}
         part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
       />
       <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        part1={part1}
+        part2={part2}
+        part3={part3}
       />
     </div>
   );
