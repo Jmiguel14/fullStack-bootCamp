527a4ad2cc11e9487fd01e8e8e83f2ea2b54f1c0
Jmiguel14 <miguel.jurado@epn.edu.ec>
2021-10-06 19:14:04 -0500

    2.9 the phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,35 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", phone: '0929939884' }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filteredPersons, setFilteredPersons] = useState(persons)
+  const [wordToFilter, setWordToFilter] = useState('')
 
+  console.log(persons)
   const handleSubmit = (e) => {
     e.preventDefault();
     const existsName = persons.find((el) => el.name === newName);
-    typeof existsName !== "undefined"
-      ? alert(`${newName} already exists in the phonebook`)
-      : setPersons((prevState) => [...prevState, { name: newName, phone: newNumber}]);
+    if (typeof existsName !== "undefined"){
+      alert(`${newName} already exists in the phonebook`)
+    } else {
+      setFilteredPersons((prevState) => [
+        ...prevState,
+        { name: newName, number: newNumber },
+      ])
+      setPersons((prevState) => [
+        ...prevState,
+        { name: newName, number: newNumber },
+      ])
+    }
     setNewName("");
-    setNewNumber("")
+    setNewNumber("");
   };
 
   const handleNameChange = (e) => {
@@ -20,15 +37,31 @@ const App = () => {
   };
 
   const handleNumberChange = (e) => {
-    setNewNumber(e.target.value)
-  }
+    setNewNumber(e.target.value);
+  };
+
+  const handleFilterChange = (e) => {
+    const newWordToFilter = e.target.value
+    setWordToFilter(newWordToFilter)
+    const newFilteredPersons = persons.filter((person) => {
+      const nameToLowerCase = person.name.toLowerCase()
+      const valueToLowerCase = newWordToFilter.toLowerCase()
+      return nameToLowerCase.includes(valueToLowerCase)
+    }
+    );
+    setFilteredPersons(newFilteredPersons)
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>
+        filter shown with <input onChange={handleFilterChange} value={wordToFilter}/>
+      </p>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input onChange={handleNameChange} value={newName} /><br/>
+          name: <input onChange={handleNameChange} value={newName} />
+          <br />
           number: <input onChange={handleNumberChange} value={newNumber} />
         </div>
         <div>
@@ -36,8 +69,10 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>{person.name} {person.phone}</p>
+      {filteredPersons.map((person) => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
       ))}
     </div>
   );
