4259e437b22d3d423bd479f538243dc117bc9c0a
Jmiguel14 <miguel.jurado@epn.edu.ec>
2021-10-20 16:19:24 -0500

    2.11 the phonebook step6

--- part2/phonebook/src/Components/App.js
+++ part2/phonebook/src/Components/App.js
@@ -1,24 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Filter } from "./Filter";
 import { PersonForm } from "./PersonForm";
 import { Persons } from "./Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filteredPersons, setFilteredPersons] = useState(persons);
   const [wordToFilter, setWordToFilter] = useState("");
+  console.log('render')
+  console.log(persons, filteredPersons)
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log(response.data)
+        setPersons(response.data)
+        setFilteredPersons(response.data)
+      })
+  }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const existsName = persons.find((el) => el.name === newName);
-    if (typeof existsName !== "undefined") {
+    const existsName = persons.some((el) => el.name === newName);
+    if (existsName) {
       alert(`${newName} already exists in the phonebook`);
     } else {
       setFilteredPersons((prevState) => [
